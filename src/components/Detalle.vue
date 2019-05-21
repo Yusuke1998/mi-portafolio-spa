6da17d550ea8191fb4ea796878b2af2a43353564
Yusuke1998 <jhonnyjose1998@gmail.com>
2019-05-20 20:56:26 -0400

    Agregada funcionalidad para mostrar multiples imagenes y urls en el detalle de proyecto

--- src/components/Detalle.vue
+++ src/components/Detalle.vue
@@ -3,10 +3,22 @@
 		<h2 v-text="tituloProyecto"></h2>
 		<blockquote class="blockquote text-center">
 		  <p v-text="descripcionProyecto" class="mb-0"></p>
-		  <footer class="blockquote-footer">Repositorio
-		  	<a :href="urlProyecto" target="_blank">
-		  		<cite title="Ir al proyecto" v-text="urlProyecto"></cite>
-		  	</a>
+		  <footer class="blockquote-footer">
+		  	<span v-if="urlProyecto.src">
+		  	Repositorios
+			  	<div v-for="item,key in urlProyecto.src">
+			  		<a :href="item" target="_blank">
+			  			{{ key+1 }})
+			  			<cite title="Ir al proyecto" v-text="item"></cite>
+			  		</a>
+				</div>
+		  	</span>
+		  	<span v-else>
+		  	Repositorio
+			  	<a :href="urlProyecto" target="_blank">
+			  		<cite title="Ir al proyecto" v-text="urlProyecto"></cite>
+			  	</a>
+		  	</span>
 		  	<hr>
 		  	<a class="h6" href="#" @click="showHerramientas = !showHerramientas">Herramientas de desarrollo.</a>
 		  	<transition name="herramientas">
@@ -14,7 +26,18 @@
 		  	</transition>
 		  </footer>
 		</blockquote>
-		<img :src="imagenProyecto" class="img-fluid mb-5" alt="Imagen de proyecto">
+		<!-- Si tiene varias imagenes -->
+		<div v-if="imagenProyecto.src">
+			<div v-for="item,key in imagenProyecto.src">
+				{{ key+1 }}) 
+				<img :src="item" class="img-fluid" alt="Imagen de proyecto"><hr>
+			</div>
+		</div>
+		<!-- Si solo tiene una imagen -->
+		<div v-else>
+			<img :src="imagenProyecto" class="img-fluid mb-5" alt="Imagen de proyecto">
+		</div>
+
 	</div>
 </template>
 <script>
